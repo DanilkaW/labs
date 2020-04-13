0a200769a01b0358ea575ba90bbddf11c56604c1
almat3010 <61385356+almat3010@users.noreply.github.com>
2020-04-13 22:31:31 +0600

    Add files via upload

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,93 @@
+import data.Student
+import data.studentList
+import kotlinx.html.*
+import kotlinx.html.attributes.enumEncode
+import kotlinx.html.dom.append
+import kotlinx.html.js.*
+import kotlinx.html.js.li
+import org.w3c.dom.HTMLElement
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.events.Event
 import kotlin.browser.document
-import kotlin.js.Date
+import kotlin.dom.clear
+
+var ascending = true
 
 fun main() {
-    val data = Date()
-    val today = data.getTime()
-    val year = data.getFullYear()
-    val day1 = Date(year+1,0,1).getTime()
-    document.write("Today is  $data ")
-    val mlsec = (day1-today)/(1000 * 60 * 60 * 24)
-    document.write("<br> ${mlsec.toInt()} days until The new year")
+    document.getElementById("root")!!
+        .append {
+            h1 {
+                +"Students"
+                onClickFunction = onCLickFunction()
+            }
+            ol {
+                attributes += "id" to "listStudents"
+                studentList.map {
+                    li {
+                        attributes += "id" to it.firstname
+                        +"${it.firstname} ${it.surname}"
+                        onClickFunction = presenceFun(it)
+                    }
+                }
+            }
+                input(options = arrayListOf("white","blue","red"))
+        }
 }
+
+
+private fun LI.presenceFun(Student :Student): (Event) -> Unit {
+    return{
+        val student = document.getElementById(Student.firstname)!!
+        if (Student.presence) {
+            student.setAttribute("style", "color:grey")
+            Student.presence = false
+        }
+        else {
+            student.setAttribute("style", "color:white")
+            Student.presence = true
+        }
+    }
+}
+
+private fun H1.onCLickFunction (): (Event) -> Unit {
+    return {
+        val listStudents = document.getElementById("listStudents")!!
+        listStudents.clear()
+        listStudents.append {
+            if (ascending) {
+                studentList.sortBy { it.firstname }
+            }
+            else
+                studentList.sortByDescending { it.firstname }
+            ascending = !ascending
+
+            studentList.map {
+                li {
+                    attributes += "id" to it.firstname
+                    +"${it.firstname} ${it.surname}"
+                    onClickFunction = presenceFun(it)
+                    if(!it.presence)
+                        attributes += "style" to "color: grey"
+                }
+            }
+        }
+    }
+}
+
+fun TagConsumer<HTMLElement>.input(options : List<String>,
+                                   classes : String? = null,
+                                   block : P.() -> Unit = {})
+        : HTMLElement = p(classes) {
+    options.forEach {
+        +it
+        input (InputType.radio, name = "colors"){
+            value = it
+            onClickFunction = {
+                val color =  document.getElementById("root")!!
+                color.setAttribute("style","color: ${value}")
+            }
+        }
+    }
+    block()
+}
+
